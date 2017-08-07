3ac5f37e963c3369ffb67bb38955966df39a1a46
Ether <etherling@gmail.com>
2017-08-06 22:38:09 -0700

    Checkpoint 29 Landing Page: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,38 +1,28 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
-
-// var animatePoints = function() {
-    // var points = document.getElementsByClassName('point');
-    function revealPoint() {
-        for (var i = 0; i < 3; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransfrom = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+var animatePoints = function() {
+    var revealPoint = function() {
+        // #7
+        $(this).css({
+            opacity: 1;
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-
-    revealPoint();
-
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
-    // alert("The window has loaded!");
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+    // #1
+    if ($(window).height() > 950) {
+         animatePoints();
     }
-
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-    window.addEventListener('scroll', function(event) {
-        // console.log(event);
-        // console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+    // #2
+    var scrollDistance = $('sellingPoints').offset().top - $(window).height + 200;
+    // #3
+    $(window).scroll(function(event) {
+        // #4
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
         }
     });
-}
+});
 
 animatePoints();
