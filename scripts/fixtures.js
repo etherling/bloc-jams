a3b8439ad3d99d7adeea5e90e891ed1dfdeff27a
Ether <etherling@gmail.com>
2017-08-25 17:37:39 -0700

    Checkpoint 32 Next Previous completed but not working

---
+++ scripts/fixtures.js
@@ -0,0 +1,53 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+
+
+// Third Example Album
+var albumMJ = {
+    title: 'Thriller',
+    artist: 'Michael Jackson',
+    label: 'Epic - CBS',
+    year: '1982',
+    albumArtUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/55/Michael_Jackson_-_Thriller.png/220px-Michael_Jackson_-_Thriller.png',
+    songs: [
+        { title: "Wanna Be Startin' Somethin'", duration: '6:04' },
+        { title: 'Baby Be Mine', duration: '4:21' },
+        { title: 'The Girl Is Mine', duration: '3:43'},
+        { title: 'Thriller', duration: '5:58' },
+        { title: 'Beat It', duration: '4:18'},
+        { title: 'Billie Jean', duration: '4:54' },
+        { title: 'Human Nature', duration: '4:07' },
+        { title: 'P.Y.T. (Pretty Young Thing)', duration: '3:58' },
+        { title: 'The Lady in My Life', duration: '4:59' }
+    ]
+};
